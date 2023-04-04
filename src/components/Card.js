928d6cfcabfcc2afba861bc3a36437b7af7008a7
Nurasyl <nurasylqazaqwws@gmail.com>
2023-04-05 02:19:24 +0600

    final checking

--- src/components/Card.js
+++ src/components/Card.js
@@ -30,7 +30,7 @@ const Card = () => {
       <motion.div
         style={{ x, y, rotateX, rotateY, z: 100 }}
         drag
-        dragElastic={0.18}
+        dragElastic={0.20}
         dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
         whileTap={{cursor:'grabbing'}}
         className='w-[426px] min-h-[600px] bg-[#e4dfdc] rounded-[30px] border-[4px] border-white px-[40px] py-[24px] cursor-grab relative' >
@@ -62,7 +62,9 @@ const Card = () => {
           }
         </ul >
         {/* card image */}
-        < motion.div className='absolute top-12 -right-64 w-[620px]' >
+        < motion.div 
+        style={{ x, y, rotateX, rotateY, z: 100000 }}
+        className='absolute top-12 -right-64 w-[620px]' >
           <img src={NikeImg} alt="" draggable='false' />
         </motion.div >
 
